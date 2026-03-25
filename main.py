547dca42960e3a6b1466d61ebe57a9adad983c52
mkyildizzz <akarmustafa511@gmail.com>
2026-03-26 00:18:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-data = [77, 69, 82, 83, 73, 78]
+data = [154, 138, 164, 166, 146, 156]
 
-decoded = "".join([chr(x) for x in data])
+decoded = "".join([chr(x - 77) for x in data])
 
-print("location:", decoded)
+print(decoded)
